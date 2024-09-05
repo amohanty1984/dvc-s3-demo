c85cb78b44fe04e02dd9a8b76a0b265da40b9045
amohanty1984 <2017ht66049@wilp.bits-pilani.ac.in>
2024-09-05 18:21:02 +0530

    initial commit with pc2

---
+++ src/data_ingestion.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.decomposition import PCA
+
+import os
+
+df = pd.read_csv('C:/Users/user/Documents/campusX/2_ML-OPS/Session-13/self-dcv-s3-demo/data/raw/student_performance_dataset.csv')
+#data\raw\student_performance_dataset.csv
+
+# Separating features and target variable
+
+X = df.drop(columns=['Placed'])
+y = df['Placed']
+
+# Scaling the data
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Applying PCA
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X_scaled)
+
+# Creating a DataFrame with PCA results
+df_pca = pd.DataFrame(data=X_pca, columns=['PC1', 'PC2'])
+df_pca['Placed'] = y.values
+
+df_pca.to_csv(os.path.join('data', 'processed', 'student_performance_pca.csv'), index=False)
